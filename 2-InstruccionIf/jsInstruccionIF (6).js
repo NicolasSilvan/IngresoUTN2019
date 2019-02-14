fa69daf5af315c3a7bdd1e4adcf3929c05de0945
NicolasSilvan <46906373+NicolasSilvan@users.noreply.github.com>
2019-02-14 05:53:13 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -2,9 +2,20 @@ function Mostrar()
 {
 //tomo la edad  
 
+var edad = parseInt(document.getElementById("edad").value);
 
-
-
+if (edad >= 18) {
+    alert("Es mayor de edad")
+    
+}
+if (edad < 13) {
+    alert("Es un niño")
+    
+}
+if (edad >= 13 && edad <= 17) {
+    alert("Es un adolescente")
+    
+}
 
 
 }//FIN DE LA FUNCIÓN
