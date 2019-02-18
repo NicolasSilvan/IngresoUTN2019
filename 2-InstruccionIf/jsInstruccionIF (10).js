aca5334d22ff69ac0011f50011c95f07ef745c01
NicolasSilvan <46906373+NicolasSilvan@users.noreply.github.com>
2019-02-18 19:13:04 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -2,5 +2,19 @@ function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+	var num = Math.floor(Math.random() * 10 + 1);
+
+    alert(num);
+
+    if (num > 4 && num < 9) {
+		alert("APROBO")
+		
+	} else if (num >= 9 && num <= 10) {
+		alert("EXCELENTE")
+		
+	} else if (num <= 4) {
+		alert("Vamos, la proxima se puede")
+
+	}
 
 }//FIN DE LA FUNCIÓN
