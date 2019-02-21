4f9b8896a5976cb2bbf414d623cd03f38f63d6b3
NicolasSilvan <46906373+NicolasSilvan@users.noreply.github.com>
2019-02-21 12:06:47 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -1,10 +1,11 @@
-function Mostrar()
-{
+function Mostrar() {
 
-var sexo = prompt("ingrese f ó m .");
+    var sexo = prompt("ingrese f ó m .").toLowerCase();
 
+    while (sexo != "f" && sexo != "m") {
+        sexo = prompt("Error. Reingrese f o m.").toLowerCase;
+    }
 
-
-document.getElementById('Sexo').value=sexo;
+    document.getElementById('Sexo').value = sexo;
 
 }//FIN DE LA FUNCIÓN
