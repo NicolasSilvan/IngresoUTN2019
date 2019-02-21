64223420dbbe36d13e363a6f9a240e5771b22dd2
NicolasSilvan <46906373+NicolasSilvan@users.noreply.github.com>
2019-02-21 12:26:41 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -3,8 +3,8 @@ function Mostrar() {
 	var numero = prompt("ingrese un número entre 0 y 10.");
     numero = parseInt(document.getElementById("Numero").value);
 
-	while (!(numero >= 0 && numero <= 10)) {
-		prompt("Error. Reingresar numero entre 0 y 10")
+	while (numero < 0 && numero > 10) {
+		prompt("Error. Reingrese un numero entre 0 y 10");
 	}
 	document.getElementById("Numero").value = numero;
 
