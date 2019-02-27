1f1b52ec783af62661a3771dde3fa13431cbdce0
NicolasSilvan <46906373+NicolasSilvan@users.noreply.github.com>
2019-02-27 12:27:20 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -1,16 +1,21 @@
-function Mostrar()
-{
+function Mostrar() {
 
-	var contador=0;
-	var positivo=0;
-	var negativo=1;
-	
-	var respuesta='si';
+	var contador = 0;
+	var positivo = 0;
+	var negativo = 1;
+	var numero;
+	var respuesta = 'si';
 
-	
+	do {
+
+		numero = parseInt(prompt("Ingrese un numero"));
+
+		contador = contador + numero;
+		contador++;
+	} while ( a);
 
 
-document.getElementById('suma').value=positivo;
-document.getElementById('producto').value=negativo;
+	document.getElementById('suma').value = positivo;
+	document.getElementById('producto').value = negativo;
 
 }//FIN DE LA FUNCIÓN
